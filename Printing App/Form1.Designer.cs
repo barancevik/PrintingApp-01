1be289b5554113340f4178c52f91a1409c2d6f0e
Baran ÇEVİK <barancevik1@gmail.com>
2024-06-21 22:13:02 +0300

    Printing App-01
    Application for printing the product code entered in the button

---
+++ Printing App/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Printing_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbPrinterslist = new System.Windows.Forms.ComboBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choise Printers";
+            // 
+            // cbPrinterslist
+            // 
+            this.cbPrinterslist.FormattingEnabled = true;
+            this.cbPrinterslist.Location = new System.Drawing.Point(224, 35);
+            this.cbPrinterslist.Name = "cbPrinterslist";
+            this.cbPrinterslist.Size = new System.Drawing.Size(192, 28);
+            this.cbPrinterslist.TabIndex = 1;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.BackColor = System.Drawing.Color.Green;
+            this.btnPrint.Location = new System.Drawing.Point(224, 107);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(192, 54);
+            this.btnPrint.TabIndex = 2;
+            this.btnPrint.Text = "KTM CODE 5";
+            this.btnPrint.UseVisualStyleBackColor = false;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 317);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.cbPrinterslist);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbPrinterslist;
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
+
---
+++ Printing App/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+
+namespace Printing_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (PrinterSettings.InstalledPrinters.Count<=0)
+            {
+                MessageBox.Show("Printer Not Found!");
+                return;
+
+            }
+
+
+            foreach (String printer in PrinterSettings.InstalledPrinters)
+            {
+                cbPrinterslist.Items.Add(printer.ToString());
+            }
+
+
+
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.PrinterSettings.PrinterName = cbPrinterslist.SelectedItem.ToString();
+            pd.PrintPage += new PrintPageEventHandler(pd_PrintPage);
+            pd.Print();
+
+
+
+        }
+
+        private void pd_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            //throw new NotImplementedException();
+            Graphics g = e.Graphics;
+            Font font = new Font("Arial",16);
+            SolidBrush brush = new SolidBrush(Color.Black);
+           // g.DrawString("Hello print",font,brush, new Rectangle(20,20,100,100));
+            g.DrawString(btnPrint.Text, font, brush, new Rectangle(20, 20, 100, 100));
+
+
+        }
+    }
+}
